cabf795978869d729e3f2a2db9e93ec9b2472cdd
James Bart <jbart1@gmail.com>
2024-12-19 16:56:08 +0000

    First push

---
+++ index.js
@@ -0,0 +1,116 @@
+const express = require('express')
+const app = express()
+app.use(express.json())
+app.use(express.static('dist'))
+const cors = require('cors')
+app.use(cors({ origin: 'http://localhost:5173' }))
+const morgan = require('morgan')
+require('dotenv').config()
+
+morgan.token('body', req => {
+  return JSON.stringify(req.body)
+})
+
+app.use(morgan(':method :url :status :response-time ms :body'))
+const mongoose = require('mongoose')
+const url = process.env.MONGODB_URI
+console.log(url)
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+
+const Note = require('./models/note')
+
+var currentDate = new Date()
+var currentDateString = currentDate.toString()
+
+
+
+app.get('/info', (request, response) => {
+  Note.countDocuments({}).then(count => {
+    response.send(`Phonebook has info for ${count} notes </br> ${currentDateString}`)
+  })
+})
+
+app.get('/api/notes', (request, response) => {
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
+})
+
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
+
+app.delete('/api/notes/:id', (request, response,next) => {
+
+  Note.findByIdAndDelete(request.params.id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+  if (body.content === undefined) {
+    return response.status(400).json({ error: 'content missing' })
+  }
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  })
+
+
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
+})
+
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
